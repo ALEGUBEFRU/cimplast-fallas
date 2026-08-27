b7861dbbdc1c66840d68271f1cba33a0a46b0f2a
ALEGUBEFRU <alexisfrutos2@gmail.com>
2026-08-27 09:25:08 -0300

    Add files via upload

--- datos.js
+++ datos.js
@@ -6,7 +6,7 @@
 
 window.CIMPLAST_DATA = (function() {
 
-  const API = 'https://script.google.com/macros/s/AKfycbzjQaOth-SdhVjZGZ6zKj9cWeVy8jiHLUp7lJpOIQXYCKwkhDDGnGA97KjH5qSdgou1HA/exec';
+  const API = 'https://script.google.com/macros/s/AKfycbwtK4LH8nmMePsh6Yfas-lMGZcJ-KZuRS3JmZuT-29ouihH-VQWU2K5s2d2VmV2lyFCSg/exec';
 
   const PLANTAS = ["Planta 3", "Planta 4", "Planta 5", "Titese", "Matricería", "Otros"];
 
